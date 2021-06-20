070f26a326b04ad87ad6cb6f143004ffc585f3dc
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2021-06-20 13:24:54 +0530

    ted talks collection added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,112 @@
 import "./styles.css";
 
+const tedTalksDataBase = {
+  Technology: [
+    {
+      speaker: "Aaron Morris",
+      title: "The future diagnostic lab ... inside your body",
+      postedOn: "Jun 2021",
+      thumbnail:
+        "https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/a18fbf7c-0ea1-4769-9fd7-d200183c7c16/AaronMorris_2021F-embed.jpg?quality=89&w=320",
+      link:
+        "https://www.ted.com/talks/aaron_morris_the_future_diagnostic_lab_inside_your_body"
+    },
+    {
+      speaker: "Brittany Young",
+      title: "How dirt bikes and STEM ignite ingenuity in Baltimore",
+      postedOn: "May 2021",
+      thumbnail:
+        "https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/ac48e77a-2cc5-4fd5-aee0-f8fb5c856efe/BrittanyYoung_2021F-embed.jpg?quality=89&w=320",
+      link:
+        "https://www.ted.com/talks/brittany_young_how_dirt_bikes_and_stem_ignite_ingenuity_in_baltimore"
+    },
+    {
+      speaker: "Beth Ford",
+      title: "What farmers need to be modern, climate-friendly and profitable",
+      postedOn: "May 2021",
+      thumbnail:
+        "https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/4bc4978d-f052-46b7-8cb3-6bbb1e7b4c3b/BethFord_2021S-embed.jpg?quality=89&w=320",
+      link:
+        "https://www.ted.com/talks/beth_ford_what_farmers_need_to_be_modern_climate_friendly_and_profitable"
+    },
+    {
+      speaker: "Nadya Bartol",
+      title: "Better cybersecurity starts with honesty and accountability",
+      postedOn: "May 2021",
+      thumbnail:
+        "https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/416bc029-1d1a-4be9-adf1-21180227765e/NadyaBartol_2021S-embed.jpg?quality=89&w=320",
+      link:
+        "https://www.ted.com/talks/nadya_bartol_better_cybersecurity_starts_with_honesty_and_accountability"
+    },
+    {
+      speaker: "Kalika Bali",
+      title: "The giant leaps in language technology — and who's left behind",
+      postedOn: "Apr 2021",
+      thumbnail:
+        "https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/f546d184-50e3-4226-bea6-ae8c09647d0b/KalikaBali_2020X-embed.jpg?quality=89&w=320",
+      link:
+        "https://www.ted.com/talks/kalika_bali_the_giant_leaps_in_language_technology_and_who_s_left_behind"
+    }
+  ],
+  Design: [
+    {
+      speaker: "Matthew Mazzotta",
+      title:
+        "Playful, wondrous public spaces built for community and possibility",
+      postedOn: "Jun 2021",
+      thumbnail:
+        "https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/e221f0c5-860c-43f6-93ea-242fca6d76f5/MatthewMazzotta_2021F-embed.jpg?quality=89&w=320",
+      link:
+        "https://www.ted.com/talks/matthew_mazzotta_playful_wondrous_public_spaces_built_for_community_and_possibility"
+    },
+    {
+      speaker: "Jorge Mañes Rubio",
+      title: "What should humans take to space (and leave behind)?",
+      postedOn: "May 2021",
+      thumbnail:
+        "https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/a2edb612-f915-4fdf-9369-0f5fd06894ef/JorgeManesRubio_2021F-embed.jpg?quality=89&w=320",
+      link:
+        "https://www.ted.com/talks/jorge_manes_rubio_what_should_humans_take_to_space_and_leave_behind"
+    },
+    {
+      speaker: "Daniel Lismore",
+      title: "My life as a work of art",
+      postedOn: "Jun 2019",
+      thumbnail:
+        "https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/3e67c7b1-3b79-4911-a867-f7e8c8aed2b3/DanielLismore_2019-embed.jpg?quality=89&w=320",
+      link: "https://www.ted.com/talks/daniel_lismore_my_life_as_a_work_of_art"
+    },
+    {
+      speaker: "Martina Flor",
+      title: "The secret language of letter design",
+      postedOn: "Dec 2017",
+      thumbnail:
+        "https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/a5b2cda0-ae5a-4c4b-aabc-3119f03828f2/MartinaFlor_2016X-embed.jpg?quality=89&w=320",
+      link:
+        "https://www.ted.com/talks/martina_flor_the_secret_language_of_letter_design"
+    },
+    {
+      speaker: "Arvind Gupta",
+      title: "Turning trash into toys for learning",
+      postedOn: "Apr 2011",
+      thumbnail:
+        "https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/7d61a626-335b-40fc-b2bd-bcb8c9deec26/ArvindGupta_2010P-embed.jpg?quality=89&w=320",
+      link:
+        "https://www.ted.com/talks/arvind_gupta_turning_trash_into_toys_for_learning"
+    }
+  ]
+};
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Edit to see some magic happen!</h2>
+      <h1>
+        <span style={{ color: "red" }}>TED</span>-Talks
+      </h1>
+      <p>
+        <strong>3700+ talks</strong> to stir your curiosity
+      </p>
+      <small style={{ display: "block" }}>Find just the right one</small>
     </div>
   );
 }
